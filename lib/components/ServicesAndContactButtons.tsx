87c45cef2119275ece2eb2868f6cdaa2380198a7
hperezs <h.perez.saldivar@gmail.com>
2025-09-10 12:43:51 -0600

    Missed these buttons

--- lib/components/ServicesAndContactButtons.tsx
+++ lib/components/ServicesAndContactButtons.tsx
@@ -2,12 +2,12 @@ import Link from "next/link";
 export const ServicesAndContactButtons = () => {
   return (
     <div className="flex justify-center w-full gap-6 mt-6">
-      <Link href={`${process.env.PAGES_BASE_PATH}/#services`}>
+      <Link href={`/#services`}>
         <button className="cursor-pointer px-6 py-2 bg-blue-700 hover:bg-blue-800 text-white font-semibold rounded shadow transition-colors duration-200">
           Ver Más Servicios
         </button>
       </Link>
-      <Link href={`${process.env.PAGES_BASE_PATH}/#contact`}>
+      <Link href={`/#contact`}>
         <button className="cursor-pointer px-6 py-2 bg-green-700 hover:bg-green-800 text-white font-semibold rounded shadow transition-colors duration-200">
           Contacto
         </button>
